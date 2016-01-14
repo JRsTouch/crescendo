69dfb4c50f2d81504ab12756eadbf953d93e3003
David <David2205@orange.fr>
2016-01-14 10:28:39 +0100

    Intégration des slider video et photo

--- public/assets/js/javascript.js
+++ public/assets/js/javascript.js
@@ -1,5 +1,20 @@
 $(function(){
 
+	$(window).load(function() {
+	  $('#slider').flexslider({
+	    animation: "slide"
+	  });
+	});
 
+	$(window).load(function() {
+	  $('#carousel').flexslider({
+	    animation: "slide",
+	    animationLoop: false,
+	    itemWidth: 210,
+	    itemMargin: 5,
+	    minItems: 2,
+	    maxItems: 4
+	  });
+	});
 
 });
