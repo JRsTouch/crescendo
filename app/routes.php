4c3d6a49451830ee44d70c34ec70436fe4a41309
Julien RALETZ <raletz@hotmail.fr>
2016-01-18 15:15:34 +0100

    routes

--- app/routes.php
+++ app/routes.php
@@ -15,11 +15,10 @@
 		
 		// Partie Privée
 		['GET', '/choristes', 'Choristes#Home', 'choristes_home'],
-<<<<<<< HEAD
-		['GET|POST', '/choristes/ajout_news', 'Choristes#Ajout', 'choriste_ajout_news']
-=======
+
+		['GET|POST', '/choristes/ajout_news', 'Choristes#Ajout', 'choriste_ajout_news'],
 		['GET', '/choristes/ajout_news', 'Choristes#Ajout', 'choristes_news'],
 		['GET', '/choristes/chansons', 'Choristes#Chansons', 'choristes_chansons'],
->>>>>>> julien-chansons
+
 
 	);
