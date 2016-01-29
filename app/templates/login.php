55d4b6ec817c19e905d7305932933b0e6896de00
David <David2205@orange.fr>
2016-01-29 11:32:22 +0100

    Animation sur la page home

--- app/templates/login.php
+++ app/templates/login.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title><?= $this->e($title) ?></title>
+	<link rel="shortcut icon" href="<?= $this->assetUrl('img/favicon.ico') ?>" />
 	<script src="<?= $this->assetUrl('js/jquery-2.1.4.min.js') ?>"></script>
 	<link href='https://fonts.googleapis.com/css?family=PT+Sans' rel='stylesheet' type='text/css'>
 	<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
